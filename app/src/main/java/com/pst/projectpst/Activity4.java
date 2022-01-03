d0d8d10fc5a72ca81f05a82d05a1eb5d8b7514ae
PauloToro <ptoro@espol.edu.ec>
2022-01-03 17:22:04 -0500

    Creada Activity 4 con la finalidad de que en esta se pueda abrir la camara del celular y realizar la lectura del codigo QR.

--- app/src/main/java/com/pst/projectpst/Activity4.java
+++ app/src/main/java/com/pst/projectpst/Activity4.java
@@ -4,11 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Activity4 extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class Activity4 extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView escanerView;
+    private String nombreCaja;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4);
+    }
+
+    public void EscanerQR(View view){
+        escanerView=new ZXingScannerView(this);
+        setContentView(escanerView);
+        escanerView.setResultHandler(this);
+        escanerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        escanerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        nombreCaja=result.getText();
+        escanerView.resumeCameraPreview(this);
+
     }
 }
